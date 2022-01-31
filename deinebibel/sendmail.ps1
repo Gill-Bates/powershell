93d1c31c292f9b8bcd6711b77bdc06d13b485965
Your Name <you@example.com>
2022-01-31 22:30:31 +0100

    Add Postgres Support

--- deinebibel/sendmail.ps1
+++ deinebibel/sendmail.ps1
@@ -2,28 +2,46 @@
 .SYNOPSIS
 .DESCRIPTION Script to send Confirmation E-Mails
 .LINK https://github.com/Gill-Bates/powershell
-.NOTES Last Update: 17.01.2022
+.NOTES Last Update: 31.01.2022
 #>
 
-# Install-Module -Scope CurrentUser -Name SimplySql -Force
+# Install-Module -Name SimplySql -Force
 Import-Module SimplySql
 
+# Select Database Mode!
+$databaseMode = "postgres" # or "mysql"
+
 #region staticVariables
-[string]$mailuser = "web253p7"
+[string]$mailUser = "web253p7"
 [string]$sender = "deinebibel.eu <noreply@deinebibel.eu>"
 [string]$smtpServer = "server4.webgo24.de"
 [string]$bcc = "tobias@steiner.rs"
-[string]$workingDir = "/root/powershell"
+[string]$workingDir = "/root/pwsh/deinebibel"
 [string]$confirmMailBody = "./mail_confirm.htm"
 [string]$denyMailBody = "./mail_deny.htm"
-
-# Database Setup
-[string]$username = "deinebibel"
-[string]$server = "localhost"
-[int]$port = 3333
 [string]$database = "deinebibel"
-[string]$dbtable = "Bestellungen"
+[string]$dbtable = "bestellungen"
+
+### DATABASE CONFIG ###
+if ($databaseMode -Like "mysql") {
+
+    # Database Setup mySQL
+    [string]$username = "deinebibel"
+    [string]$server = "localhost"
+    [int]$port = 3333
+    [string]$database = "deinebibel"
+    [string]$dbtable = "bestellungen"
+    #endregion
+}
+elseif ($databaseMode -Like "postgres") {
+    # Database Setup postgres
+    [string]$username = "deinebibel"
+    [string]$server = "localhost"
+    [int]$port = 5555
+    #endregion
+}
 #endregion
+
 
 ##### Function #######
 
@@ -93,29 +111,42 @@ function Send-MailNotification {
     }
 }
 
-##### PROGRAM AREA #####
+################## DATABASE AREA ##################
 
+Write-Output ""
+Write-Warning "*** Running in Database-Mode '$databaseMode'! ***" -WarningAction Continue
+Write-Output ""
 $dbpasswd = Get-Password -File "$workingDir/pw_deinebibel.txt"
 $dbcredential = New-Object System.Management.Automation.PSCredential ($username, $dbpasswd)
 
 # Open SQL Connection
-Open-mySqlConnection -SSLMode Required -Server $server -Database $database -Credential $dbcredential -Port $port
+if ($databaseMode -Like "mysql") {
 
+    Open-mySqlConnection -SSLMode Required -Server $server -Database $database -Credential $dbcredential -Port $port
+}
+elseif ($databaseMode -Like "postgres") {
+
+    Open-PostGreConnection -Server $server -Port $port -Credential $Credential -TrustSSL -Database $database
+}
+else {
+    throw "No valid Database-Mode selected! Check your config!"
+}
+
+# Check Connection
 if (!($conn = Get-SqlConnection)) {
     throw "[ERROR] Can't open SQL-Connection!"
 }
 else {
-    Write-Output "[OK] SQL-Connection to '$($conn.DataSource)' successful established!"
+    Write-Output "[OK] SQL-Connection to '$($conn.DataSource)' successfully established!"
 }
 
 ################## CONFIRM ODERS ##################
 
-$query = "SELECT * FROM $dbtable WHERE Sent = 1"
+$query = "SELECT * FROM $dbtable WHERE sent = '1'"
 $recipients = Invoke-Sqlquery -Query $query
 
 if (!$recipients) { 
     Write-Output "[OK] No Orders found to notify!"
-    Write-Output ""
 }
 else {
     Write-Output "Found '$($recipients.Count)' Orders!"
@@ -125,8 +156,8 @@ $count = 1
 $recipients | ForEach-Object {
 
     Write-Output "[$count/$($recipients.Count)] Send Mail to '$($_.Email)' ..."
-    $mailpasswd = Get-Password -File "$workingDir/pw_noreply.txt"
-    [pscredential]$MailCredential = New-Object System.Management.Automation.PSCredential ($mailuser, $mailpasswd)
+    $mailPasswd = Get-Password -File "$workingDir/pw_web253p7.txt"
+    [pscredential]$MailCredential = New-Object System.Management.Automation.PSCredential ($mailUser, $mailPasswd)
     Send-MailNotification -Credential $MailCredential -UserPrincipalName $_.userPrincipalName -NotificationSend $notificationSend -Confirm
 
     # Update Database
@@ -140,37 +171,39 @@ $recipients | ForEach-Object {
 
 ################## DENY ODERS ##################
 
-$query = "SELECT * FROM $dbtable WHERE Sent = 2"
+$query = "SELECT * FROM $dbtable WHERE sent = '2'"
 $recipients = Invoke-Sqlquery -Query $query
 
 if (!$recipients) { 
     Write-Output "[OK] No bad Orders found to deny! Exit here."
+    Write-Output ""
     Exit
 }
 else {
     Write-Output "Found '$($recipients.Count)' bad Orders!"
+    Write-Output ""
 }
 
 $count = 1
 $recipients | ForEach-Object {
 
     Write-Output "[$count/$($recipients.Count)] Send Mail to '$($_.Email)' ..."
-    $mailpasswd = Get-Password -File "$workingDir/pw_noreply.txt"
+    $mailpasswd = Get-Password -File "$workingDir/pw_web253p7.txt"
     [pscredential]$MailCredential = New-Object System.Management.Automation.PSCredential ($mailuser, $mailpasswd)
     Send-MailNotification -Credential $MailCredential -UserPrincipalName $_.userPrincipalName -NotificationSend $notificationSend -Deny
 
     # Update Database
     if ($?) {
         $timestamp = (Get-Date).ToString('yyyy-MM-dd HH:mm:ss')
-        $query = "UPDATE $dbtable SET Sent = 'DENY! ($timestamp)' WHERE ID = $($_.ID);"
+        $query = "UPDATE $dbtable SET sent = 'DENY! ($timestamp)' WHERE id = $($_.id);"
         Invoke-SqlUpdate -Query $query | Out-Null
 
         # Block E-Mail address for further orders
-        $query = "UPDATE $dbtable SET Block = '1' WHERE ID = $($_.ID);"
+        $query = "UPDATE $dbtable SET block = '1' WHERE id = $($_.id);"
         Invoke-SqlUpdate -Query $query | Out-Null
 
         # Revoke Confirmation-Token
-        $query = "UPDATE $dbtable SET ConfirmToken = NULL WHERE ID = $($_.ID);"
+        $query = "UPDATE $dbtable SET ConfirmToken = NULL WHERE id = $($_.id);"
         Invoke-SqlUpdate -Query $query | Out-Null
     }
     $count++
