706ea9d42e30a0bac97c5b66a3e16b4df807e0c9
Your Name <you@example.com>
2025-09-20 21:27:05 +0200

    Updates

--- 00_Modules/New-ModuleManifiest.ps1
+++
@@ -1,3 +0,0 @@
-$path = "C:\_Repos\Github\powershell\00_Modules\CustomLog"
-$module = (Get-CHildItem $path | Where-Object { $_.Name -like "$($path.Split("\")[-1]).psm1" }).FullName -replace (".psm1", ".psd1")
-New-ModuleManifest -Path $module
